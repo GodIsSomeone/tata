088f63ed5eb0e99e71eed4228f7ce71e0db5d0f6
GodIsSomeone <tph5559@126.com>
2018-04-19 10:18:34 +0800

    Create ReverseList.cpp

---
+++ LeetCode/ReverseList.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <xutility>
+#include <string>
+#include<algorithm>
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+    ListNode * reverseList(ListNode* head) {
+        if (head == NULL || head->next ==NULL)
+        {
+            return head;
+        }
+        ListNode *preNode = NULL;
+        ListNode *curNode = head;
+        while (curNode)
+        {
+            ListNode* nextNode = curNode->next;
+            
+            curNode->next = preNode;
+            preNode = curNode;
+            curNode = nextNode;
+        }
+        return preNode;
+    }
+};
+
+int main() {
+    ListNode *head = NULL;
+    ListNode tmpNode(1);
+    ListNode tmpNode2(2);
+    ListNode tmpNode3(3);
+    ListNode tmpNode4(4);
+    head = &tmpNode;
+    head->next = &tmpNode2;
+    tmpNode2.next = &tmpNode3;
+    tmpNode3.next = &tmpNode4;
+    ListNode * result = Solution().reverseList(head);
+    while (result)
+    {
+        cout << result->val << endl;
+        result = result->next;
+    }
+    return 0;
+}
