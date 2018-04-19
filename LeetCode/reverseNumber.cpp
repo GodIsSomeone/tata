4b097713ba19b13fefbffd34d40088db9d612d3e
GodIsSomeone <tph5559@126.com>
2018-04-19 10:19:58 +0800

    Create reverseNumber.cpp

---
+++ LeetCode/reverseNumber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+        int reverse(int x) {
+            if (x>INT_MAX || x<INT_MIN)
+            {
+                return 0;
+            }
+            long long ret = 0;
+            int temp = 0;
+            while (x)
+            {
+                temp = x%10;
+                ret = 10*ret +temp;
+                x = x /10;
+            }
+            return (ret > INT_MAX || ret < INT_MIN) ? 0 : ret;
+        }
+};
